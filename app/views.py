6633edc14f09fb4eaaeff1c581dd7bd13398275e
EC2 Default User <ec2-user@ip-172-31-9-218.us-east-2.compute.internal>
2018-10-15 12:35:06 +0000

    second commit

--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello world")
+def index2(request):
+ #   list = [1, 4, 9, 16]
+    return render(request, 'index.html', {'lists': (1,2,3,4) })
+    
+
